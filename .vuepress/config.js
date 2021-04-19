537a942873923ca547429224990f8f9454581942
ddojai <favianred@gmail.com>
2021-04-19 20:21:41 +0900

    Init theme
    - https://junilhwang.github.io/TIL/Vuepress/Theme/#vuepress-theme-%E1%84%8C%E1%85%B5%E1%86%A8%E1%84%8C%E1%85%A5%E1%86%B8-%E1%84%86%E1%85%A1%E1%86%AB%E1%84%83%E1%85%B3%E1%86%AF%E1%84%80%E1%85%B5

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  title: 'vuepress-stater', // 사이트 타이틀
-  description: 'vuepress로 만든 문서입니다.',
+  title: 'TIL',
+  description: '테마 커스텀하기',
   themeConfig: {
-    logo: 'https://avatars1.githubusercontent.com/u/18749057?s=460&v=4', // 로고 이미지
-    nav: [{ text: 'Home', link: '/' }],
-    sidebar: 'auto', // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+    search: true, // 검색은 사용하지 않는다.
+    logo: 'https://avatars.githubusercontent.com/u/2128592?v=4', // logo 이미지
+    nav: [
+      { text: 'Home', link: '/' }, // 상단 메뉴
+    ],
+    sidebar: 'auto', // 사이드바 사용 여부. auto로 하면 content의 내용이 반영된다.
+    smoothScroll: true, // 부드러운 스크롤 사용 여부
   },
 };
---
+++ .vuepress/theme/layouts/GlobalLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="global-layout">
+    <component :is="$page.path ? 'Layout' : 'NotFound'" />
+    <footer class="siteFooter">
+      &copy; 김흥열
+    </footer>
+  </div>
+</template>
